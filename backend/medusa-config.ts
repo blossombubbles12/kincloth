216f6df4dd5492749f7a4404975825c508da3093
blossombubbles12 <blossombubbles12@users.noreply.github.com>
2026-05-04 14:17:19 +0100

    feat: add medusa backend

---
+++ backend/medusa-config.ts
@@ -0,0 +1,41 @@
+import { loadEnv, defineConfig } from '@medusajs/framework/utils'
+
+loadEnv(process.env.NODE_ENV || 'development', process.cwd())
+
+module.exports = defineConfig({
+  projectConfig: {
+    databaseUrl: process.env.DATABASE_URL,
+    databaseDriverOptions: { 
+      connection: { 
+        ssl: { rejectUnauthorized: false } 
+      } 
+    },
+    http: {
+      storeCors: process.env.STORE_CORS!,
+      adminCors: process.env.ADMIN_CORS!,
+      authCors: process.env.AUTH_CORS!,
+      jwtSecret: process.env.JWT_SECRET || "supersecret",
+      cookieSecret: process.env.COOKIE_SECRET || "supersecret",
+    }
+  },
+  modules: [
+    {
+      resolve: "@medusajs/medusa/file",
+      options: {
+        providers: [
+          {
+            resolve: "@ridoy_sarker/medusa-cloudinary/providers/cloudinary",
+            id: "cloudinary",
+            options: {
+              apiKey: process.env.CLOUDINARY_API_KEY,
+              apiSecret: process.env.CLOUDINARY_API_SECRET,
+              cloudName: process.env.CLOUDINARY_CLOUD_NAME,
+              folderName: "chiangel",
+              secure: true,
+            },
+          },
+        ],
+      },
+    },
+  ],
+})
